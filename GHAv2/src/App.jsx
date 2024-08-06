ba441cc77035e6f911840620dfb84c149fd66e31
Brian-Tas <brianjtascone@gmail.com>
2024-08-06 18:44:05 -0400

    Installed React.js and Tailwind.css

---
+++ GHAv2/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Title from "./Components/Title/Title.jsx";
+
+function App() {
+  return (
+    <div className="page-container">
+      <Title/>
+    </div>
+  );
+}
+
+export default App
