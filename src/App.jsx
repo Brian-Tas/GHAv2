e0a64ac44af3cf74926a36055b14065e0c406ffe
Brian-Tas <brianjtascone@gmail.com>
2024-08-07 14:58:31 -0400

    changed things

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//Pages
+import Homepage from "./Pages/Homepage";
+import Thermometry from './Pages/Thermometry';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homepage/>}/>
+          <Route path='/Thermometry' element={<Thermometry/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
